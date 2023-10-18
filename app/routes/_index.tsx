fc45ca054b2e3de593276eae09a8a67def03df94
Ellisande <justin.dragos@gmail.com>
2023-10-18 12:04:55 -0700

    Add a css stylesheet to the index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,12 +1,22 @@
+import { PrismaClient } from "@prisma/client";
 import type { MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { Card } from "~/components/Card";
+import landingStylesUrls from "~/styles/landing.css";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "High Steaks" },
+    { name: "description", content: "Luxury hats made of premium meats" },
   ];
 };
 
+export const links = () => [{ rel: "stylesheet", href: landingStylesUrls }];
+
 export default function Index() {
-  return <div style={{ fontSize: "100px", color: "red" }}>Hello world</div>;
+  return (
+    <div className="my-5 mx-10">
+      <h1 className="hero my-5">High Steaks</h1>
+    </div>
+  );
 }
