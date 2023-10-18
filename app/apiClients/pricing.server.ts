b911c4e52c1a91f860c1afc24a5ec5b39dc0bee6
Ellisande <justin.dragos@gmail.com>
2023-10-18 12:14:40 -0700

    Finish pricing implementation

--- app/apiClients/pricing.server.ts
+++ app/apiClients/pricing.server.ts
@@ -1,5 +1,55 @@
-export const getPrice = async (productId: number, size: string) => {
+type PricingInfo = {
+  basePrice: number;
+  totalPrice: number;
+  lineItems: {
+    description: string;
+    price: number;
+  }[];
+  fees: {
+    description: string;
+    price: number;
+  }[];
+};
+
+export const getPrice = async (
+  productId: number,
+  size: string
+): Promise<PricingInfo> => {
   // Super slow pricing API
-  await new Promise((r) => setTimeout(r, 5000));
-  return Math.random() * 400 + 100;
+  await new Promise((r) => setTimeout(r, 3000));
+  const basePrice = Math.random() * 400 + 100;
+  const lineItems = [
+    {
+      description: "Butchering",
+      price: Math.random() * 40,
+    },
+    {
+      description: "Tailoring",
+      price: Math.random() * 20,
+    },
+    {
+      description: "Millinering",
+      price: Math.random() * 100,
+    },
+  ];
+  const fees = [
+    {
+      description: "Taxes",
+      price: Math.random() * 20,
+    },
+    {
+      description: "Mercurcy disposal fee",
+      price: Math.random() * 5,
+    },
+  ];
+  const totalPrice =
+    basePrice +
+    lineItems.reduce((a, b) => a + b.price, 0) +
+    fees.reduce((a, b) => a + b.price, 0);
+  return {
+    basePrice,
+    totalPrice,
+    lineItems,
+    fees,
+  };
 };
