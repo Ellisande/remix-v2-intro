b9ec93e41cdc4d9745b40ba892d2952b463093df
Ellisande <justin.dragos@gmail.com>
2023-10-18 12:13:49 -0700

    Add a static image to the index page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,5 @@
-import { PrismaClient } from "@prisma/client";
 import type { MetaFunction } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { Card } from "~/components/Card";
+import hat1Url from "~/images/hat_1.png";
 import landingStylesUrls from "~/styles/landing.css";
 
 export const meta: MetaFunction = () => {
@@ -17,6 +15,14 @@ export default function Index() {
   return (
     <div className="my-5 mx-10">
       <h1 className="hero my-5">High Steaks</h1>
+      <div className="grid grid-cols-3 gap-3">
+        <div className="flex flex-col relative text-transparent hover:text-white transition-colors">
+          <img src={hat1Url} />
+          <h2 className=" font-bold absolute text-8xl w-full text-center top-1/2 ">
+            Ribeye on the Range
+          </h2>
+        </div>
+      </div>
     </div>
   );
 }
