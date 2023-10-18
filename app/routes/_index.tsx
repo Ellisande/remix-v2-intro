2fe9035bbe77e6668ac6f2971e90c254faf95adb
Ellisande <justin.dragos@gmail.com>
2023-10-18 12:17:59 -0700

    Added product loader to index Added card component to render products

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
+import { PrismaClient } from "@prisma/client";
 import type { MetaFunction } from "@remix-run/node";
-import hat1Url from "~/images/hat_1.png";
+import { useLoaderData } from "@remix-run/react";
+import { Card } from "~/components/Card";
 import landingStylesUrls from "~/styles/landing.css";
 
 export const meta: MetaFunction = () => {
@@ -11,17 +13,20 @@ export const meta: MetaFunction = () => {
 
 export const links = () => [{ rel: "stylesheet", href: landingStylesUrls }];
 
+export const loader = async () => {
+  const products = await new PrismaClient().product.findMany();
+  return { products };
+};
+
 export default function Index() {
+  const { products } = useLoaderData<typeof loader>();
   return (
     <div className="my-5 mx-10">
       <h1 className="hero my-5">High Steaks</h1>
       <div className="grid grid-cols-3 gap-3">
-        <div className="flex flex-col relative text-transparent hover:text-white transition-colors">
-          <img src={hat1Url} />
-          <h2 className=" font-bold absolute text-8xl w-full text-center top-1/2 ">
-            Ribeye on the Range
-          </h2>
-        </div>
+        {products.map((product) => (
+          <Card product={product} />
+        ))}
       </div>
     </div>
   );
