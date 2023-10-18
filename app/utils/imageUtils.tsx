e7a9bf83aa12c68c8a8ccb6f6eaf90123beaea2e
Ellisande <justin.dragos@gmail.com>
2023-10-18 13:08:06 -0700

    Add an lookup util for static images

---
+++ app/utils/imageUtils.tsx
@@ -0,0 +1,13 @@
+import hat1 from "../images/hat_1.png";
+import hat2 from "../images/hat_2.png";
+import hat3 from "../images/hat_3.png";
+import hat4 from "../images/hat_4.png";
+import hat5 from "../images/hat_5.png";
+
+export const slugUrlMap: { [key: string]: string } = {
+  hat_1: hat1,
+  hat_2: hat2,
+  hat_3: hat3,
+  hat_4: hat4,
+  hat_5: hat5,
+};
